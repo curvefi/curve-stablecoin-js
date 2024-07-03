308ece6a9a0b0896ea2dc04a96cf990f04f4f5b8
fedorovdg <fedorovdgap@gmail.com>
2024-07-03 19:30:57 +0400

    fix: fixed userLoss api response handle

--- src/llammas/LlammaTemplate.ts
+++ src/llammas/LlammaTemplate.ts
@@ -466,7 +466,7 @@ export class LlammaTemplate {
     public async userLoss(userAddress = ""): Promise<{ deposited_collateral: string, current_collateral_estimation: string, loss: string, loss_pct: string }> {
         userAddress = _getAddress(userAddress);
         const [deposited_collateral, _current_collateral_estimation] = await Promise.all([
-            _getUserCollateral(crvusd.constants.NETWORK_NAME, this.controller, userAddress, this.collateralDecimals),
+            _getUserCollateral(crvusd.constants.NETWORK_NAME, this.controller, userAddress),
             crvusd.contracts[this.address].contract.get_y_up(userAddress),
         ]);
         const current_collateral_estimation = crvusd.formatUnits(_current_collateral_estimation, this.collateralDecimals);
